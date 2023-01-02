e553abfdd5f97928387da1f809cb2f2cb9143c93
Umang <umang2singh@gmail.com>
2023-01-02 21:24:24 +0530

    Microsoft Q1

---
+++ Microsoft/src/Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+// Link- https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+public class Evaluate_Reverse_Polish_Notation {
+
+    // We will use stack here as for every arithmetic operator, we have to operate it on previous 2 operands
+    class Solution {
+        public int evalRPN(String[] tokens) {
+
+            Stack<Integer> stack = new Stack<>();
+
+            for (String s : tokens) {
+
+                if ("+".equals(s)) stack.push(stack.pop() + stack.pop());
+
+                else if ("-".equals(s)) stack.push(-stack.pop() + stack.pop());
+
+                else if ("/".equals(s)) stack.push((int)(1D / stack.pop() * stack.pop()));
+
+                else if ("*".equals(s)) stack.push(stack.pop() * stack.pop());
+
+                else stack.push(Integer.valueOf(s));
+
+            }
+
+            return stack.pop();
+        }
+    }
+
+}
